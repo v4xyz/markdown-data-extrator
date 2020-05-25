2d1e0e4c31ef396aa4c7cd5b8fbb16e9e9db61ea
v4xyz <v4xyz@126.com>
2020-05-25 08:30:25 +0800

    完成md表格数据提取

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ const marked = require('marked')
 const yfm = require('yaml-front-matter')
 const $ = require('cheerio')
 
+/**
+* 获取table数据
+*/
 function getTableData (table) {
+	// cherrioz只能使用jquery兼容方法 不能使用node.innerText 这种标准DOM属性	
+  const headers = $(table).find('thead tr th').map((i, node) => $(node).text()).get()
+  const bodys =  $(table).find('tbody tr').map((i, node) => node).get()
+  	.map(tr => $(tr).find('td').map((i, node) => $(node).text()).get())
 
 
-}
+  // return $(table).find('thead tr').children()
+  return bodys.map(item => {
+
+  	return headers.reduce((acc, field, index) => {
+
+  		return {...acc,
+  			[field]: item[index]
+  		}
+  	}, {})
+  })
+}  
 
 fs.readFile('./test01.md', (err, data) => {
 	
 	const htmlContent = marked(data.toString())
 	console.log(htmlContent)
 	const tables = $.load(htmlContent)('table')
-	console.log($(tables[0]).text())
+	console.log(getTableData(tables[0]))
 
 })
 
